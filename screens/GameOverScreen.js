c8df7bb23a5c7e1c995e10e5b092af9fc3a78521
Erick Perez <erickeperez24@gmail.com>
2020-11-09 18:01:55 -0500

    Network Image Added
    Added network image and styled the container of the image as well as the text summary when you lose.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,14 +2,33 @@ import React from 'react';
 import {View, Text, StyleSheet, Button, Image} from 'react-native';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
+import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <TitleText> The Game is Over</TitleText>
-            <Image source={require('../assets/success.png')} />
-            <BodyText> Number of rounds: {props.roundsNumber}</BodyText>
-            <BodyText> Your Guess was number: {props.userNumber}</BodyText>
+            <View style={styles.imageContainer}>
+                {/* <Image 
+                source={require('../assets/success.png')} 
+                style={styles.image}
+                resizeMode='cover'
+                /> */}
+                <Image 
+                source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT39RCTYAerd2mtIXNGV3y2KPZfMuTSZ1tV3Q&usqp=CAU'}} 
+                style={styles.image}
+                resizeMode='cover'
+                />
+            </View>
+            <View style={styles.resultContainer}>
+                <BodyText style={styles.resultText}>
+                Your phone needed{' '}
+                <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+                guess the number{' '}
+                <Text style={styles.highlight}>{props.userNumber}</Text>.
+                </BodyText>
+            </View>
+
             <Button title="New Game" onPress={props.onRestart} />
         </View>
     );
@@ -20,6 +39,31 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    }, 
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    imageContainer: {
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        width: 300,
+        height: 300,
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    resultContainer: {
+        marginHorizontal: 30,
+        marginVertical: 15
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold',
     }
 });
 
