e3766a63854469f414fc2d0784164921d0e2fa88
Erick Perez <erickeperez24@gmail.com>
2020-11-16 12:35:48 -0500

    Responsive Styling
    Added some responsive styling to the beginning buttons of our game.
    
    Used Dimension.get('window').width to get the actual screen device.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import {View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Alert, Dimensions } from 'react-native';
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
@@ -93,8 +93,9 @@ const styles = StyleSheet.create({
         fontFamily: 'open-sans'
     },
     inputContainer: {
-        width: 300,
-        maxWidth: '80%',
+        width: '80%',
+        minWidth: 300,
+        maxWidth: '95%',
         alignItems: 'center'
     },
     buttonContainer: {
@@ -104,7 +105,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15
     },
     button: {
-        width: 100
+        width: Dimensions.get('window').width / 4
     },
     input: {
         width: 50,
